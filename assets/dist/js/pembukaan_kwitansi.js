7efaffdefd84b2a1d8c05b6436c4c0eb0b800d7b
baihaqisuhaeri <baihaqisuhaeri.im@gmail.com>
2022-11-14 15:15:00 +0700

    14 November 2022

--- assets/dist/js/pembukaan_kwitansi.js
+++ assets/dist/js/pembukaan_kwitansi.js
@@ -1,3 +1,5 @@
+var kd_unit_global;
+
 $(document).ready(function () {
     get_customer();
   });
@@ -6,6 +8,64 @@ $(".select2").select2({ width: "100%" });
 $(".select2bs4").select2({
   theme: "bootstrap4",
 });
+
+$("#nama_customer").on("change", function () {
+  $("#error_nama_customer").html("");
+    var id_cust = $("#nama_customer").val();
+    $.ajax({
+        url: "permohonan-kwitansi/get-nama-customer",
+        type: "post",
+        data: { id_cust: id_cust },
+        success: function (data) {
+           // console.log(id_cust);
+            data = JSON.parse(data);
+          $("#kode_customer").val(data.kode_customer);
+          set_tgl_mohon();
+          kd_unit_global = data.kd_unit;
+        },
+      });
+
+      
+  });
+
+
+  $(document).on("click", "#btn_tambah_detail", function () {
+    // sampe sini 13 Oktober 2022 
+    //id_array = [];
+    var err = 0;
+    var k_cus = $("#kode_customer").val();
+    //console.log(k_cus);
+    if (k_cus == "") {
+      $("#error_nama_customer").html(
+        "Customer harus dipilih terlebih dahulu"
+      );
+      err += 1;
+    }
+    if (err == 0) {
+      
+      $("#modal_detail").modal("show");
+      $("#tabel_sj_detail").dataTable().fnDestroy();
+      table = $("#tabel_sj_detail").DataTable({
+        //scrollX: true,
+        processing: true, //Feature control the processing indicator.
+        serverSide: true, //Feature control DataTables' server-side processing mode.
+        order: [], //Initial no order.
+    
+        // Load data for the table's content from an Ajax source
+        ajax: {
+          url: "permohonan-kwitansi/get-sj-detail",
+          type: "POST",
+          data: { k_cus: k_cus,
+                  kd_unit: kd_unit_global
+                   },
+        },
+    
+      });
+     
+    }
+      
+      
+    });
 
 //List of function
 
@@ -19,3 +79,5 @@ function get_customer() {
       },
     });
   }
+
+  
